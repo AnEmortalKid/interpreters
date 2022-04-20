a1cf77a3c8a8fcb2329718cdb67b8fe09296c004
AnEmortalKid <janmonterrubio@gmail.com>
2022-04-19 19:19:35 -0500

    finish chap 12

--- jlox/src/main/java/io/anemortalkid/lox/Interpreter.java
+++ jlox/src/main/java/io/anemortalkid/lox/Interpreter.java
@@ -1,12 +1,15 @@
 package io.anemortalkid.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /** Syntax Tree -> WOW */
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   private Environment globals = new Environment();
+  private final Map<Expr, Integer> locals = new HashMap<>();
 
   private static class BreakException extends RuntimeException {}
 
@@ -51,11 +54,21 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     statement.accept(this);
   }
 
+  void resolve(Expr expr, int depth) {
+    locals.put(expr, depth);
+  }
+
   @Override
   public Object visitAssignExpr(Expr.Assign expr) {
     Object value = evaluate(expr.value);
 
-    environment.assign(expr.name, value);
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      environment.assignAt(distance, expr.name, value);
+    } else {
+      globals.assign(expr.name, value);
+    }
+
     return value;
   }
 
@@ -161,7 +174,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Object visitVariableExpr(Expr.Variable expr) {
-    return environment.get(expr.name);
+    return lookUpVariable(expr.name, expr);
   }
 
   @Override
@@ -328,5 +341,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     return function.call(this, arguments);
+  }
+
+  private Object lookUpVariable(Token name, Expr expr) {
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      return environment.getAt(distance, name.lexeme);
+    } else {
+      return globals.get(name);
+    }
   }
 }
